37cc2a6ef45f0cad9fa472acb8394be486b6f0ec
Bharat Bhushan Verma <bharat.verma@smu.ca>
2018-11-27 16:28:47 -0400

    SQL Script V1.0
    SQL DB tables creation attempt

---
+++ tables.sql
@@ -0,0 +1,16 @@
+USE ANON;
+GO
+CREATE TABLE Users (
+  UserID INT,
+  EmailID VARCHAR(50),
+  Fname VARCHAR(50),
+  Lname VARCHAR(50),
+  StreetNumber VARCHAR(50),
+  City VARCHAR(50),
+  Province VARCHAR(50),
+  Country VARCHAR(50),
+  Pcode VARCHAR(50),
+  PhoneNumber INT,
+  UserType VARCHAR(10)
+);
+GO
