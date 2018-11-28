888cbfcb62d14fc971b991d18c830888f5810218
Bharat Bhushan Verma <bharat.verma@smu.ca>
2018-11-28 13:06:41 -0400

    Division Added

--- Calculator_A00428533/src/Calculator.java
+++ Calculator_A00428533/src/Calculator.java
@@ -1,46 +1,54 @@
 import java.util.Scanner;
 
 public class Calculator {
-	
+
 	public static void add(int a,int b)
-	{   
+	{
 		int res=0;
 		res = a + b;
 		System.out.println(res);
 	}
-	
+
 	public static void subtract(int a,int b)
-	{   
+	{
 		int res=0;
 		res = a - b;
 		System.out.println(res);
 
 	}
 public static min(int a,int b)
-	{   
+	{
 		int res=0;
 		res = Math.min(a,b);
 		System.out.println(res);
 
 	}
 public static max(int a,int b)
-	{   
+	{
 		int res=0;
 		res = Math.max(a,b);
 		System.out.println(res);
 
 	}
-	
+
 		public static void multiply(int a,int b)
-	{   
+	{
 		int res=0;
 		res = a * b;
 		System.out.println(res);
 
 	}
-	
+
+	public static void divide(int a,int b)
+{
+	int res=0;
+	res = a / b;
+	System.out.println(res);
+
+}
+
 	public static void main(String args[]) {
-	
+
 	 int a, b, res;
      Scanner scan = new Scanner(System.in);
      System.out.print("Enter Two Numbers : ");
@@ -48,9 +56,9 @@ public static max(int a,int b)
      b = scan.nextInt();
      Calculator.add(a,b);
      Calculator.add(a,b);
-     
-     
 
-     
+
+
+
 	}
 }
