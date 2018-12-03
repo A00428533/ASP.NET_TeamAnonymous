5267e0799ee029c0cfd7809f664205312e7db3c5
A00429842 <Zewei.Yan@smu.ca>
2018-12-04 05:47:11 +0800

    trxn model

---
+++ WebApplication5/WebApplication5/Models/Trxn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Trxn
+    {
+        public string id { get; set; }
+        public string nameOnCard { get; set; }
+        public string cardNumber { get; set; }
+        public string expDate { get; set; }
+        public string createdOn { get; set; }
+        public string createdBy { get; set; }
+        public string CreditCardTypeName { get; set; }
+        public double unitPrice { get; set; }
+        public double totalPrice { get; set; }
+        public int quantity { get; set; }
+        public int cardCategory { get; set; }
+        public int ReservationId { get; set; }
+        public int UserId { get; set; }
+    }
+}
